afb5f4c4c862a18739d8d187509aa6b26e82c5a9
Masakazu Matsumoto <vitroid@gmail.com>
2022-09-10 21:50:26 +0900

    Svelte app is separated.

--- svelte-app/src/Components/Tabs/TabList.svelte
+++
@@ -1,9 +0,0 @@
-<div class="tab-list">
-	<slot></slot>
-</div>
-
-<style>
-	.tab-list {
-		border-bottom: 3px solid #5ab4bd;
-	}
-</style>
--- svelte-app/src/localeselector.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import { locale,locales } from 'svelte-i18n';
-</script>
-
-<select bind:value={$locale}>
-    {#each $locales as locale}
-        <option value={locale}>{locale}</option>
-    {/each}
-</select>
